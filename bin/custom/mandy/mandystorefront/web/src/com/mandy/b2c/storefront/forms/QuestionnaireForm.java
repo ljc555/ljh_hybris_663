ef74ac5449042c9ebd1368c6c70c7ec0125e1b1a
jianghong.li <ljc555@gmail.com>
2018-05-02 17:01:56 +0800

    create Questionnaire model

---
+++ bin/custom/mandy/mandystorefront/web/src/com/mandy/b2c/storefront/forms/QuestionnaireForm.java
@@ -0,0 +1,82 @@
+package com.mandy.b2c.storefront.forms;
+
+/**
+ * @author ljh
+ * @version 1.0.0
+ * @PackageName com.mandy.b2c.storefront.forms
+ * @ClassName QuestionnaireForm
+ * @Description 问卷调查Form
+ * @Date 2018/5/2 15:50
+ */
+public class QuestionnaireForm {
+    /** 调查Code **/
+    private String code;
+    /** 调查productCode **/
+    private String productCode;
+    /** 调查productName **/
+    private String productName;
+    /** 价格 **/
+    private Double price;
+    /** 产品厂家 **/
+    private String manufactor;
+    /** 信息来源 **/
+    private String source;
+    /** 是否购买 **/
+    private Boolean ifBuy;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getManufactor() {
+        return manufactor;
+    }
+
+    public void setManufactor(String manufactor) {
+        this.manufactor = manufactor;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public Boolean getIfBuy() {
+        return ifBuy;
+    }
+
+    public void setIfBuy(Boolean ifBuy) {
+        this.ifBuy = ifBuy;
+    }
+}
